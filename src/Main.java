33e6ec284055790dd436615709b88fab873c869f
thisisshivam31 <thisisshivam31@gmail.com>
2021-08-19 01:58:44 +0530

    Done This

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Math.max(5, 10));
+        int n = 5;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++){
+
+                System.out.print("x ");
+            }
+            System.out.println("x ");
+        }
+
     }
 }
--- src/bhbn.java
+++ src/bhbn.java
@@ -1,4 +1,5 @@
 public class bhbn {
-
-
+    public static void main(String[] args) {
+        System.out.println(Math.min(15, 10));
+    }
 }
