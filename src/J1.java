eb4e3bcfc087aaa24a4d7fb7e22eabd172d67f9b
thisisshivam31 <thisisshivam31@gmail.com>
2021-08-19 02:28:31 +0530

    Done This

--- src/J1.java
+++ src/J1.java
@@ -1,5 +1,7 @@
+//JAVA program to print a pattern of right triangle using  star
+
 import java.util.*;
-public class Main {
+public class J1 {
     public static void main(String[] args) {
         int i, j, k=5;
         for(i=1; i<=k; i++){
