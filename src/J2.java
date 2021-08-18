79eee9047acbe707d951287d2bd65d6f614df6a3
thisisshivam31 <thisisshivam31@gmail.com>
2021-08-19 02:32:20 +0530

    done

---
+++ src/J2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class J2 {
+    public static void main(String[] args) {
+        int i, j, k=5;
+        for(i=1; i<=k; i++){
+            for (j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
