8b77093c461475f10d56c1dfc5f0864a5f868d97
SHIVAM SINGH <49497648+thisisshivam31@users.noreply.github.com>
2021-11-25 00:57:13 +0530

    Create odd_even.java

---
+++ src/odd_even.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner SC = new Scanner(System.in);
+	    int a = SC.nextInt();
+	    if (a/2==0){
+	        System.out.println("even");
+	    }
+	    else{
+		System.out.println("Odd");
+	    }
+	}
+}
