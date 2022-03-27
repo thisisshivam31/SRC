3769c4b8f9a75efed6701db4f5591deedc547b6e
SHIVAM SINGH <49497648+thisisshivam31@users.noreply.github.com>
2022-03-27 23:21:15 +0530

    Create 31_SquarePattern.java

---
+++ src/31_SquarePattern.java
@@ -0,0 +1,27 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    for(int i=0; i<5; i++){
+	        for(int j=0; j<5; j++){
+		    System.out.print("*");
+	        }
+		System.out.println("*");
+	    }
+	}
+}
+
+/* [14] Tringle pattern code 12.*/
+
+
+public class Main{
+	public static void main(String[] args) {
+	    for(int i=1; i<=5; i++){
+	        
+	        for(int j=1; j<i; j++){
+		    System.out.print(" *");
+	        }
+	        
+		System.out.println(" *");
+	    }
+	}
+}
