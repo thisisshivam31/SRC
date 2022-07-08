3df04170c816e6c445cab482bb61f434e5b76045
SHIVAM SINGH <49497648+thisisshivam31@users.noreply.github.com>
2022-07-09 00:20:17 +0530

    Update 2_pattern_of_square.java

--- src/2_pattern_of_square.java
+++ src/2_pattern_of_square.java
@@ -5,7 +5,7 @@ public class J2 {
     public static void main(String[] args) {
         int i, j, k=5;
         for(i=1; i<=k; i++){
-            for (j=1; j<=i; j++){
+            for (j=1; j<=k; j++){
                 System.out.print("* ");
             }
             System.out.println(" ");
